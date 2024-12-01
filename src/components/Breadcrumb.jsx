23e6e8ead269388c3b36958dabd628ec81683645
正版青尘 <201436728@qq.com>
2024-12-01 23:13:48 +0800

    fix: Add key for Breadcrumb component

--- src/components/Breadcrumb.jsx
+++ src/components/Breadcrumb.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { StyleSheet, View, Text, Pressable } from 'react-native';
 
 const styles = StyleSheet.create({
@@ -30,14 +30,14 @@ export default function Breadcrumb({ style, path, setPath }) {
     return (
         <View style={[styles.container, style]}>
             {path.map((item, index) => (
-                <>
+                <Fragment key={index}>
                     {index > 0 && <Text style={styles.separator}>&gt;</Text>}
                     <Pressable style={styles.item} onPress={() => setPath(path.slice(0, index + 1))} android_ripple={{
                         color: 'lightgray',
                     }}>
                         <Text style={[styles.itemLabel, index === path.length - 1 && styles.itemLabelActive]}>{item}</Text>
                     </Pressable>
-                </>
+                </Fragment>
             ))}
         </View>
     );
